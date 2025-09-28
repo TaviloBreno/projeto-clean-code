62c7b6f85afdec8a8268cb75295c843bc1e5dce6
Tavilo Breno <breno_wk2@hotmail.com>
2025-09-28 18:44:40 -0300

    fix: update user profile repository import in change profile picture factory

--- src/main/factories/use-cases/change-profile-picture.ts
+++ src/main/factories/use-cases/change-profile-picture.ts
@@ -1,8 +1,8 @@
 import { setupChangeProfilePicture } from '@/data/services/change-profile-picture'
 import { type ChangeProfilePicture } from '@/domain/use-cases/change-profile-picture'
 import { makeAwsS3FileStorage } from '@/main/factories/infra/aws-s3-file-storage'
-import { makePgUserProfileRepository } from '@/main/factories/repos/pg-user-profile'
+import { makeUserProfileRepo } from '@/main/factories/repos/user-profile'
 
 export const makeChangeProfilePicture = (): ChangeProfilePicture => {
-  return setupChangeProfilePicture(makeAwsS3FileStorage(), makePgUserProfileRepository())
+  return setupChangeProfilePicture(makeAwsS3FileStorage(), makeUserProfileRepo())
 }
