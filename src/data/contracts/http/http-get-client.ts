8f66ab58cc186eab2c48eca7759177f671d09e30
Tavilo Breno <breno_wk2@hotmail.com>
2025-09-28 15:38:41 -0300

    feat: add HttpGetClient interface and axios dependency

---
+++ src/data/contracts/http/http-get-client.ts
@@ -0,0 +1,10 @@
+export interface HttpGetClient {
+  get: <T = any>(params: HttpGetClient.Params) => Promise<T>
+}
+
+export namespace HttpGetClient {
+  export interface Params {
+    url: string
+    params?: Record<string, string | number | boolean>
+  }
+}
